2b8509c3f5dfb99f309acc8ac13eef4cf0777f98
Junior Alves <jrnalves@gmail.com>
2025-05-26 17:10:57 -0300

    crinado o tiltulo e a desrição no header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,36 @@ export default function Home() {
     <div className="min-h-dvh max-w-screen">
       <header>
         <div className="max-w-7xl mx-auto">
-          <figure className="max-w-3xl h-120 relative aspect-video justify-self-end">
-            <Image
-              src="/images/leticia-martins.png"
-              fill
-              alt="Leticia Martins"
-              className="rounded-xl absolute object-cover object-[25%_25%]"
-            />
-          </figure>
 
-          <div>
-            <span className="text-lg font-bold text-purple-1000">
-              Software Engineer
-            </span>
+          <div className="flex justify-end-safe">
+            <figure className="relative w-full max-w-3xl aspect-video overflow-hidden rounded-xl">
+              <Image
+                src="/images/leticia-martins.png"
+                alt="Leticia Martins"
+                fill
+                className="object-cover object-[25%_25%]"
+                sizes="(max-width: 768px) 100vw, 1200px"
+              />
+            </figure>
+          </div>
 
-            <h1 className="font-anton text-9xl font-bold text-purple-1000">
-              Leticia Martins
-            </h1>
+
+          <div className="flex flex-nowrap gap-20 min-h-52">
+            <div className="flex flex-col flex-wrap shrink-0 gap-2 justify-start">
+              <p className="text-lg font-ptSerif text-secondary">
+                Personal Organizer
+              </p>
+
+              <h1 className="font-anton text-9xl text-primary whitespace-pre letter-spacing-[-0.02em] leading-[1.2] font-bold">
+                Leticia Martins
+              </h1>
+            </div>
+
+            <div className="flex flex-col flex-wrap shrink gap-2 justify-end items-end-safe">
+              <p className="text-lg font-ptSerif text-secondary">
+                Sou especialista em organização de ambientes e pós-mudança. Meu trabalho é transformar espaços, unindo praticidade e harmonia para o dia a dia.
+              </p>
+            </div>
 
           </div>
         </div>
