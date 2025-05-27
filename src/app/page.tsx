537760026156ad75fbc9a5ff8e44ba327222c63c
Junior Alves <jrnalves@gmail.com>
2025-05-27 10:26:46 -0300

    adicionar navBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import NavBar from "@/components/NavBar";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="min-h-dvh max-w-screen px-2">
-      <Header />
+      <div className="max-w-6xl mx-auto">
+        <NavBar />
+        <Header />
+      </div>
     </div>
   );
 }
