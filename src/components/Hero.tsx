2510f441da0709129f55cba05c3523077b1f2bc5
Junior Alves <jrnalves@gmail.com>
2025-06-20 14:44:32 -0300

    ajustado o hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div
         className="flex justify-end-safe mt-4">
         <motion.figure
-          className="relative w-full max-w-3xl aspect-video overflow-hidden rounded-xl"
+          className="relative w-full max-w-3xl max-h-120 aspect-square overflow-hidden rounded-xl"
           initial={{ opacity: 0, transform: "translateY(100px)" }}
           animate={{ opacity: 1, transform: "translateY(0px)" }}
           transition={{
@@ -21,7 +21,7 @@ export default function Hero() {
             src="/images/leticia-martins.png"
             alt="Leticia Martins"
             fill
-            className="object-cover object-[25%_25%]"
+            className="object-cover object-[25%_25%] w-full h-full"
             sizes="(max-width: 768px) 100vw, 1200px"
           />
         </motion.figure>
@@ -42,7 +42,7 @@ export default function Hero() {
             Personal Organizer
           </p>
 
-          <h1 className="font-anton text-clamp text-primary whitespace-pre letter-spacing-[-0.02em] leading-[1.2] font-bold lg:text-8xl">
+          <h1 className="font-anton text-clamp text-primary whitespace-pre letter-spacing-[-0.02em] leading-[1.2] font-bold lg:text-8xl xl:text-9xl">
             Leticia Martins
           </h1>
         </motion.div>
@@ -67,8 +67,9 @@ export default function Hero() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <li className="text-lg font-ptSerif bg-zinc-100 p-2.5 rounded-full group-hover:bg-purple-400 transition-all">
-                <FaInstagram className="text-zinc-600 text-2xl group-hover:text-white transition-all" />
+              <li className="text-lg font-ptSerif bg-zinc-100 p-2.5 rounded-full group-hover:bg-purple-400 transition-all duration-300">
+                <FaInstagram
+                  className="text-zinc-600 text-2xl group-hover:text-white transition-all duration-300" />
               </li>
             </Link>
 
@@ -78,8 +79,9 @@ export default function Hero() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <li className="text-lg font-ptSerif bg-zinc-100 p-2.5 rounded-full group-hover:bg-purple-400 transition-all">
-                <FaWhatsapp className="text-zinc-600 text-2xl group-hover:text-white transition-all" />
+              <li
+                className="text-lg font-ptSerif bg-zinc-100 p-2.5 rounded-full group-hover:bg-purple-400 transition-all duration-300">
+                <FaWhatsapp className="text-zinc-600 text-2xl group-hover:text-white transition-all duration-300" />
               </li>
             </Link>
           </ul>
