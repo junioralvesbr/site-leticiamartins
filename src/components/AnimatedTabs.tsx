3c070814793d23887ec613395e595b5a5109e658
Junior Alves <jrnalves@gmail.com>
2025-07-03 19:26:58 -0300

    mudança nas tabs

--- src/components/AnimatedTabs.tsx
+++ src/components/AnimatedTabs.tsx
@@ -28,17 +28,7 @@ export function TabsDemo() {
       ),
     },
     {
-      title: "Brinquedoteca",
-      value: "brinquedoteca",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Brinquedoteca organizada transforma a bagunça em diversão com harmonia.</p>
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Cozinhas",
+      title: "Cozinhas e Dispensas",
       value: "cozinha",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
@@ -48,31 +38,11 @@ export function TabsDemo() {
       ),
     },
     {
-      title: "Despensa",
-      value: "despensa",
+      title: "Banheiros & Lavanderia",
+      value: "banheiros",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Despensa organizada facilita o dia a dia e evita desperdícios.</p>
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Banheiros",
-      value: "banheiro",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Banheiros organizados trazem praticidade e bem-estar ao seu dia.</p>
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Lavanderia",
-      value: "lavanderia",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Lavanderia organizada torna as tarefas mais leves e práticas.</p>
+          <p>Cozinha organizada facilita a rotina e inspira bons momentos.</p>
           <DummyContent />
         </div>
       ),
@@ -81,7 +51,7 @@ export function TabsDemo() {
 
   return (
     <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
-      <Tabs tabs={tabs} />
+      <Tabs tabs={tabs} tabClassName="hover:cursor-pointer" />
     </div>
   );
 }
