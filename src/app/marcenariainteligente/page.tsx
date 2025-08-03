6102a4eb9db7da6fa3d76cac3de02af83d013141
Junior Alves <jrnalves@gmail.com>
2025-08-03 15:05:59 -0300

    trocado o nome da pasta consultoria para marcenaria

--- src/app/marcenariainteligente/page.tsx
+++ src/app/marcenariainteligente/page.tsx
@@ -14,28 +14,28 @@ const data = [
         </p>
         <div className="grid grid-cols-2 gap-4">
           <Image
-            src="/images/consultoria/foto-1.webp"
+            src="/images/marcenaria/foto-1.webp"
             alt="startup template"
             width={500}
             height={500}
             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
           />
           <Image
-            src="/images/consultoria/foto-2.webp"
+            src="/images/marcenaria/foto-2.webp"
             alt="startup template"
             width={500}
             height={500}
             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
           />
           <Image
-            src="/images/consultoria/foto-3.webp"
+            src="/images/marcenaria/foto-3.webp"
             alt="startup template"
             width={500}
             height={500}
             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
           />
           <Image
-            src="/images/consultoria/foto-4.jpg"
+            src="/images/marcenaria/foto-4.jpg"
             alt="startup template"
             width={500}
             height={500}
@@ -62,7 +62,7 @@ export default function MarcenariaInteligente() {
           transition={{ duration: 1 }}
         >
           <Image
-            src="/images/consultoria/foto-5.jpg"
+            src="/images/marcenaria/foto-5.jpg"
             alt="Capa comercial"
             width={800}
             height={600}
