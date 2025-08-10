f0b374988db5b884ea7d1b9035ae691d6ec5df8f
Junior Alves <jrnalves@gmail.com>
2025-08-10 13:03:58 -0300

    aria label do whatsapp

--- src/components/whatsapp.tsx
+++ src/components/whatsapp.tsx
@@ -7,6 +7,7 @@ export function WhatssAppIcon() {
       href={linksAndPaths.whatsapp.href}
       target="blank"
       className="flex flex-col fixed bottom-4 right-5 z-20 ease-in-out"
+      aria-label="Link para o Whatsapp"
     >
       <div className="bg-green-600 w-12 h-12 rounded-full flex justify-center items-center self-end">
         <FaWhatsapp className="text-white text-3xl" />
