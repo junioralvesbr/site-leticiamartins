67ca0a8e55428c9437577fc3a13a1adde3db0001
Junior Alves <jrnalves@gmail.com>
2025-10-24 09:41:42 -0300

    upgrade dos pacotes do site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev']
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  images: {
+    qualities: [25, 50, 75, 100]
+  }
 };
 
 export default nextConfig;
