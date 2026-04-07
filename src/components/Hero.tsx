1741fb7f27a8db8618d104f5436434de233c5d50
Junior Alves <jrnalves@gmail.com>
2026-04-07 19:53:07 -0300

    alterado a frase do header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,75 +1,77 @@
-import * as motion from "motion/react-client";
-import Image from "next/image";
+import * as motion from 'motion/react-client'
+import Image from 'next/image'
 
-import { linksAndPaths } from "@/data/linksAndPaths";
-import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa6";
-import SocialLinkButton from "./ui/social-link-button";
+import { linksAndPaths } from '@/data/linksAndPaths'
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa6'
+import SocialLinkButton from './ui/social-link-button'
 
 export default function Hero() {
   return (
     <div>
-      <div
-        className="flex justify-end-safe mt-4">
+      <div className='mt-4 flex justify-end-safe'>
         <motion.figure
-          className="relative w-full max-w-3xl max-h-120 aspect-square overflow-hidden rounded-xl"
-          initial={{ opacity: 0, transform: "translateY(100px)" }}
-          animate={{ opacity: 1, transform: "translateY(0px)" }}
+          className='relative aspect-square max-h-120 w-full max-w-3xl overflow-hidden rounded-xl'
+          initial={{ opacity: 0, transform: 'translateY(100px)' }}
+          animate={{ opacity: 1, transform: 'translateY(0px)' }}
           transition={{
             duration: 1,
           }}
         >
           <Image
-            src="/images/leticia-martins.png"
-            alt="Leticia Martins"
+            src='/images/leticia-martins.png'
+            alt='Leticia Martins'
             fill
             quality={100}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-cover md:object-[25%_25%] w-full h-full"
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            className='h-full w-full object-cover md:object-[25%_25%]'
             priority
-            fetchPriority="high"
+            fetchPriority='high'
           />
         </motion.figure>
       </div>
 
-
-      <section className="flex flex-col flex-nowrap gap-5 min-h-68 mt-10 xl:flex-row xl:gap-20">
+      <section className='mt-10 flex min-h-68 flex-col flex-nowrap gap-5 xl:flex-row xl:gap-20'>
         <motion.div
-          className="flex flex-col flex-wrap flex-1 shrink-0 gap-2 justify-start"
-          initial={{ opacity: 0, transform: "translateY(100px)" }}
-          animate={{ opacity: 1, transform: "translateY(0px)" }}
+          className='flex flex-1 shrink-0 flex-col flex-wrap justify-start gap-2'
+          initial={{ opacity: 0, transform: 'translateY(100px)' }}
+          animate={{ opacity: 1, transform: 'translateY(0px)' }}
           transition={{
             duration: 2,
-            delay: 0.5
+            delay: 0.5,
           }}
         >
-          <p className="text-lg font-subTitles text-fourth">
+          <p className='font-subTitles text-fourth text-lg'>
             Personal Organizer
           </p>
 
-          <span className="font-titles text-clamp text-first whitespace-pre letter-spacing-[-0.02em] leading-[1.2] font-bold lg:text-[100px] xl:text-[116px]">
+          <span className='font-titles text-clamp text-first letter-spacing-[-0.02em] leading-[1.2] font-bold whitespace-pre lg:text-[100px] xl:text-[116px]'>
             Leticia Martins
           </span>
         </motion.div>
 
         <motion.div
-          className="flex flex-col flex-wrap shrink gap-8 max-w-sm lg:pl-10 lg:self-end"
-          initial={{ opacity: 0, transform: "translateY(100px)" }}
-          animate={{ opacity: 1, transform: "translateY(0px)" }}
+          className='flex max-w-sm shrink flex-col flex-wrap gap-8 lg:self-end lg:pl-10'
+          initial={{ opacity: 0, transform: 'translateY(100px)' }}
+          animate={{ opacity: 1, transform: 'translateY(0px)' }}
           transition={{
             duration: 1.5,
-            delay: 1
+            delay: 1,
           }}
         >
-          <h1 className="text-lg font-subTitles text-fourth text-balance">
+          <h1 className='font-subTitles text-fourth text-lg text-balance'>
             Organização Residencial e Comercial em Maringá
           </h1>
-          <p className="text-lg font-subTitles text-fourth text-balance">
-            Organizar é a minha paixão: Transforme sua casa ou empresa com mais praticidade, leveza e sofisticação através da organização profissional.
+          <p className='font-subTitles text-fourth text-lg text-balance'>
+            Organizar é a minha paixão, sou personal organizer em Maringá
+            especializada em criar ambientes funcionais, organizados e elegantes
+            para o seu dia a dia.
           </p>
 
           <div>
-            <strong className="text-lg font-subTitles text-fourth text-balance">Entre em contato:</strong>
-            <ul className="flex gap-4 mt-2">
+            <strong className='font-subTitles text-fourth text-lg text-balance'>
+              Entre em contato:
+            </strong>
+            <ul className='mt-2 flex gap-4'>
               <li>
                 <SocialLinkButton
                   href={linksAndPaths.instagram.href}
